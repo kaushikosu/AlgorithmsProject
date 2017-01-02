c8a70969d9e7fa514a4974a3e72c99288a53c836
kaushiosu <kaushik2013@gmail.com>
2017-01-02 04:17:01 -0600

    added the method to retrieve suggestions for a search substring

--- src/main/java/com/kaushik/AlgorithmUtilityApp/DriverClass2.java
+++ src/main/java/com/kaushik/AlgorithmUtilityApp/DriverClass2.java
@@ -3,11 +3,14 @@ package com.kaushik.AlgorithmUtilityApp;
 import com.kaushik.AlgorithmUtility.entities.RTNode;
 
 public class DriverClass2 {
-	public static  void main(String [] args) {
-			RTNode rtNode = new RTNode("15");
-			RTNode rtNode2 = new RTNode("12");	
+	public static  void main(String [] args) {	
 			RadixTree tree = new RadixTree();
-			tree.insertNodeIntoTheTree(rtNode);
-			tree.insertNodeIntoTheTree(rtNode2);
+			tree.insertNodeIntoTheTree(new RTNode("he"));
+			tree.insertNodeIntoTheTree(new RTNode("hey"));
+			tree.insertNodeIntoTheTree(new RTNode("help"));
+			tree.insertNodeIntoTheTree(new RTNode("hell"));
+			tree.insertNodeIntoTheTree(new RTNode("hello"));
+			
+			System.out.println(tree.suggestionsForASubString("hel"));
 	}
 }
--- src/main/java/com/kaushik/AlgorithmUtilityApp/RadixTree.java
+++ src/main/java/com/kaushik/AlgorithmUtilityApp/RadixTree.java
@@ -1,6 +1,7 @@
 package com.kaushik.AlgorithmUtilityApp;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -38,6 +39,7 @@ public class RadixTree implements Tree {
 			rootOfLastInsert = insertCharacterIntoTheTree(rootOfLastInsert,letter);
 		}
 		rootOfLastInsert.setVisible(true);
+		System.out.println("node value is "+rootOfLastInsert.getValue());
 	}
 	
 	public RTNode insertCharacterIntoTheTree(RTNode rootOfLastInsert, char c){
@@ -46,7 +48,6 @@ public class RadixTree implements Tree {
 			RTNode destination = new RTNode(destinationNodeValue);
 			rootOfLastInsert.addEdge(destination,c);
 			System.out.println("added edge with weight "+c);
-			System.out.println("destination node value is "+destinationNodeValue);
 			rootOfLastInsert = destination;
 		}
 		else{
@@ -56,6 +57,42 @@ public class RadixTree implements Tree {
 		return rootOfLastInsert;
 	}
 
+	public List<String> suggestionsForASubString(String subString){
+		char[] letters = subString.toCharArray();
+		RTNode pointer = root;
+		for (char letter: letters){
+			RTEdge edge = pointer.findMatchingEdge(letter);
+			if (edge == null){
+				return Collections.emptyList();
+			}
+			pointer = (RTNode)edge.getDestination();
+		}
+		return getAllStringsUnderANode(pointer);
+	}
+	
+	private List<String> getAllStringsUnderANode(RTNode r){
+		List<String> strings = new ArrayList<String>();
+		if (r.getEdges().isEmpty()){
+			if (r.isVisible()){
+				strings.add(r.getValue());
+				return strings;
+			}
+			else{
+				return Collections.emptyList();
+			}
+		}
+		else{
+			if (r.isVisible()){
+				strings.add(r.getValue());
+			}
+			for(RTEdge edge: r.getEdges()){
+				strings.addAll(getAllStringsUnderANode(((RTNode)edge.getDestination())));
+			}
+			return strings;
+		}
+		
+	}
+	
 	private List<RTNode> createNodesForInputList(List<String> inputList) {
 		List<RTNode> stringsAddedToTheTree = new ArrayList<RTNode>();
 		for(String s: inputList){
